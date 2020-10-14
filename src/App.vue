58cde3533bf8aa2103ac3778f2e7ea773d738ea1
caozhihui <caozhihui@4399inc.com>
2020-10-14 17:01:55 +0800

    文件夹初始化

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos></Todos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "@/components/Todos";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  },
-  data(){
-    return {}
+    Todos
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="todoapp">
+    <header class="header"><h1>todos</h1>
+      <input autofocus="autofocus" autocomplete="off"
+                                                 placeholder="What needs to be done?" class="new-todo"></header>
+    <section class="main" style=""><input id="toggle-all" type="checkbox" class="toggle-all">
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <input type="checkbox" class="toggle" />
+            <label>231</label>
+            <button class="destroy"></button>
+          </div>
+          <input type="text" class="edit" />
+        </li>
+      </ul>
+    </section>
+    <footer class="footer" style=""><span class="todo-count"><strong>1</strong> item left
+				</span>
+      <ul class="filters">
+        <li><a href="#/all" class="selected">All</a></li>
+        <li><a href="#/active" class="">Active</a></li>
+        <li><a href="#/completed" class="">Completed</a></li>
+      </ul>
+      <button class="clear-completed" style="">
+        Clear completed
+      </button>
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Todos'
+}
+</script>
+
+<style src="./todo.css"></style>
